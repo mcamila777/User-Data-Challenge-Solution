76b69cc1d1512311b656cb49ead051654197f603
Maria Camila <camilaalvarez7@hotmail.com>
2016-01-29 14:52:15 -0500

    Initial Commit

---
+++ User Data Challenge Solution/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  User Data Challenge Solution
+//
+//  Created by Maria Camila on 29/01/16.
+//  Copyright © 2016 mindz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
